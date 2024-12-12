3e2f79b9d88297fd7750e3b79aee4e2ad8d326ac
Victor Boussange <vic.boussange@gmail.com>
2024-12-12 10:11:09 +0100

    updated benchmark with moving window

--- benchmark/benchmark2_moving_window_lcp_distance.py
+++ benchmark/benchmark2_moving_window_lcp_distance.py
@@ -16,41 +16,40 @@ import equinox
 from tqdm import tqdm
 
 def make_raster(N=1010):
-    key = jr.PRNGKey(1)  # Random seed is explicit in JAX
-    return jr.uniform(key, (N, N), minval=0.1, maxval=0.9, dtype="float32")  # Start with a uniform permeability
+    key = jr.PRNGKey(1)
+    return jr.uniform(key, (N, N), minval=0.1, maxval=0.9, dtype="float32")
 
-def calculate_connectivity(habitat_permability, distance):
+def calculate_connectivity(habitat_permability, activities, distance):
     """For simplicitity, we calculate connectivity as the sum of the inverse of the exp of distances."""
-    # TODO: this is a hacky fix
-    # print(habitat_permability.shape)
-    activities = jnp.ones_like(habitat_permability, dtype="bool")
+
     grid = GridGraph(activities=activities, 
                      vertex_weights=habitat_permability,
                      nb_active=habitat_permability.size)
-    # TODO: this is a hacky fix
+
     window_center = jnp.array([[habitat_permability.shape[0]//2+1, habitat_permability.shape[1]//2+1]])
     dist = distance(grid, window_center)
     connectivity = jnp.sum(jnp.exp(-dist/dist.size/2))
     return connectivity
 
-connectivity_grad = equinox.filter_jit(equinox.filter_grad(calculate_connectivity)) # sensitivity to permeability
+connectivity_grad = equinox.filter_jit(equinox.filter_grad(calculate_connectivity))
 
-# TODO: not used
 run_calculation_vmap = equinox.filter_vmap(connectivity_grad)
 
-def batch_run_calculation(window_op, distance, raster, hab_qual, x_start, y_start):
-    res = run_calculation_vmap(hab_qual, distance)
+def batch_run_calculation(window_op, distance, raster, hab_qual, x_start, y_start, activities):
+    res = run_calculation_vmap(hab_qual, activities, distance)
     
     # TODO: replace by a lax.scan loop
+    # but if you do that, you need to define xx and yy as static arguments
     for i, (xx, yy) in enumerate(zip(x_start, y_start)):
         raster = window_op.add_window_to_raster(xx, yy, raster, res[i, ...])
-
     return raster
 
 if __name__ == "__main__":
-    N = 100
-    window_size = 1
-    buffer_size = 10
+    N = 1000
+    window_size = 5 # must be odd to be placed at the center
+    buffer_size = 20
+    batch_size = 1000 
+    
     result_path = Path("./results/WindowOperation2")
     result_path.mkdir(parents=True, exist_ok=True)
 
@@ -62,22 +61,30 @@ if __name__ == "__main__":
         window_size=window_size, 
         buffer_size=buffer_size
     )
-    # TODO: implement batch processing
-    batch_size = 10 # for now, we process one window at a time
+
 
     raster = jnp.zeros_like(permeability)
 
-    for window in tqdm(window_op.iterate_windows(permeability), desc="Batch progress"):
-        x_start, y_start, hab_qual = window
-        # TODO: to be replaced when using proper batches
-        hab_qual = jnp.expand_dims(hab_qual, axis=0)
-        x_start = [x_start]
-        y_start = [y_start]
-        
-        raster = batch_run_calculation(window_op, distance, raster, hab_qual, x_start, y_start)
+    for (x_start, y_start, hab_qual) in tqdm(window_op.iterate_window_batches(permeability, batch_size), desc="Batch progress"):
+        activities = jnp.ones((hab_qual.shape[0], 2*buffer_size+window_size, 2*buffer_size+window_size), dtype="bool")
+        raster = batch_run_calculation(window_op, distance, raster, hab_qual, x_start, y_start, activities)
 
     fig, ax = plt.subplots()
-    cbar = ax.imshow(raster, vmax=60)
+    cbar = ax.imshow(raster, vmax=10)
     fig.colorbar(cbar, ax=ax)
     plt.show()
     # fig.savefig(result_path / "lcp_moving_window.png", dpi=400)
+    
+    # On RTX 3090:
+
+    # N = 1000
+    # window_size = 5 # must be odd to be placed at the center
+    # buffer_size = 20
+    # batch_size = 1000 
+    # takes about 02:54min to run
+
+    # N = 1120
+    # window_size = 9 # must be odd
+    # buffer_size = 20
+    # batch_size = 500 
+    # Takes about 01:28min to run
