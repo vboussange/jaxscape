c9917823949bb03ee9bf79e61d8d135a02d9715e
Victor Boussange <vic.boussange@gmail.com>
2024-12-17 16:23:36 +0100

    annotated experiment

--- examples/experiments/pyamg/pymag.py
+++ examples/experiments/pyamg/pymag.py
@@ -1,5 +1,7 @@
 import pyamg
 import numpy as np
+
+# multigrid solver for 2D Poisson problem
 A = pyamg.gallery.poisson((500,500), format='csr')  # 2D Poisson problem on 500x500 grid
 ml = pyamg.ruge_stuben_solver(A)                    # construct the multigrid hierarchy
 print(ml)                                           # print hierarchy information
@@ -7,21 +9,18 @@ b = np.random.rand(A.shape[0])                      # pick a random right hand s
 x = ml.solve(b, tol=1e-10)                          # solve Ax=b to a tolerance of 1e-10
 print("residual: ", np.linalg.norm(b-A*x))          # compute norm of residual vector
 
-P = ml.aspreconditioner()
-P.data
-
 
 from pyamg.aggregation import smoothed_aggregation_solver
 from pyamg.gallery import poisson
 from scipy.sparse.linalg import cg
 import numpy as np
+# conjugate gradient method with AMG preconditioner
 A = poisson((500, 500), format='csr')          # matrix
 b = np.random.rand(A.shape[0])                 # random RHS
 ml = smoothed_aggregation_solver(A)            # AMG solver
 M = ml.aspreconditioner(cycle='V')             # preconditioner
 x = M(b)
 print("residual: ", np.linalg.norm(b-A*x))          # compute norm of residual vector
-
 x, info = cg(A, b, rtol=1e-5, maxiter=30, M=M)  # solve with CG
 print("residual: ", np.linalg.norm(b-A*x))          # compute norm of residual vector
 
